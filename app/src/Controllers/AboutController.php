812cffca27397c90708d330698485ac103c8b0c3
thienbg03 <bdan2300@gmail.com>
2024-10-14 13:20:53 -0400

    Update Root

--- app/src/Controllers/AboutController.php
+++ app/src/Controllers/AboutController.php
@@ -16,12 +16,19 @@ class AboutController extends BaseController
 
     public function handleAboutWebService(Request $request, Response $response): Response
     {
+        $resources  = [
+            'Rockets' => '/space-api/rockets',
+            'Space Stations' => '/space-api/spacestations',
+            'Locations' => '/space-api/locations',
+            'RocketsSpaceMission' => '/space-api/rockets/{rocketID}/missions',
+        ];
+
         $data = array(
             'api' => self::API_NAME,
             'version' => self::API_VERSION,
             'about' => 'Welcome! This i a Web service that provides information about space,planets, astronauts, missions, rockets, etc..',
             'authors' => 'Ali Ilyas, Amin Hussain, Gia Thien Bui',
-            'resources' => '/blah'
+            'resources' => $resources
         );
 
         return $this->renderJson($response, $data);
