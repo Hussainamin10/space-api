21272051ecb13aa8187a2178afda0912390774b3
thienbg03 <110068331+thienbg03@users.noreply.github.com>
2024-11-29 13:15:53 -0500

    Create env file
    For some reason the env file was deleted from the project. So, I recreated it.

--- app/config/local.prod.php
+++ app/config/local.prod.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 // Production environment
 
 return function (array $settings): array {
-    $settings['db']['database'] = 'worldcup';
+    $settings['db']['database'] = 'planeto';
     $settings['db']['hostname'] = 'localhost';
 
     return $settings;
